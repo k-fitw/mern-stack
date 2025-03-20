0aeaed1462835997819110498c7074992df32418
Khalid mohammed <22210680@student.ciu.edu.tr>
2025-03-21 01:12:17 +0200

    grasp about models

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+); // when we try to create a new document, it will automatically add a timestamp property to the document, and when it was last updated
+
+// exports the model so that it can be used in other files
+module.exports = mongoose.model("Workout", workoutSchema);
