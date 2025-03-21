8b69856115013e4053904c4bb4f8d7a1f4697f77
Khalid mohammed <22210680@student.ciu.edu.tr>
2025-03-21 14:19:14 +0200

    Added a Controller.js file to handle all the calls to the functions of workouts.js still in the process

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,31 +1,22 @@
 const express = require("express");
 const Workout = require("../models/workoutModel");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+} = require("../controllers/workoutController");
 
 //we don't have access to the app object here, so we need to create a new router object
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 //GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single workout" });
-});
+router.get("/:id", getWorkout);
 
 //POST a workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json({ workout });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-  res.json({ mssg: "POST a workout" });
-});
+router.post("/", createWorkout);
 
 //DELETE a workout
 router.delete("/:id", (req, res) => {
