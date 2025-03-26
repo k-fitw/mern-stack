bb585c08b2d3879976bcb7422c0c1776dd66691c
Khalid mohammed <22210680@student.ciu.edu.tr>
2025-03-26 23:24:59 +0200

    Added some features of the frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <p key={workout._id}>
+              <WorkoutDetails key={workout._id} workout={workout} />
+            </p>
+          ))}
+      </div>
     </div>
   );
 };
